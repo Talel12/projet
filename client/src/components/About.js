ac2d155019987563a3e1a12940543cae65d26bc3
haithem711 <49453214+haithem711@users.noreply.github.com>
2020-03-08 19:50:15 +0100

    ok

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,8 +4,11 @@ export const About = () => {
     return (
         <div>
             <h1>About us</h1>
-            <p>sfsfsf</p>
+        <p>sfsfsf</p>
             <p>fdgrebrebreberbre</p>
+
+            <p>sfsfggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggsf</p>
+
         </div>
     )
 }
